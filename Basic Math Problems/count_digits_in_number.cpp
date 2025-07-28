6a8b8e2ee51abd709864ea355d9b5f0871087d64
SajjalMalik <sajjalmalik.sm@gmail.com>
2025-07-28 19:18:08 +0500

    Added new Folder

---
+++ Basic Math Problems/count_digits_in_number.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void countDigitsInNumber(int n) {
+    int count = 0;
+    int number = n;
+    while(n > 0) {
+        int lastDigit = n % 10;
+        count++;
+        n /= 10;
+    }
+    cout << "The total number of digits in " << number << " are: " << count << endl;
+
+}
+
+int main(){
+
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    countDigitsInNumber(n);
+    
+
+    return 0;
+}
