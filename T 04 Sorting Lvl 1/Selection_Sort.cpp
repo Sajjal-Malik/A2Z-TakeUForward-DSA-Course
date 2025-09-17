668638dc3fa9281a6248efb07e81c7cdf2894c9c
SajjalMalik <sajjalmalik.sm@gmail.com>
2025-09-17 23:55:17 +0500

    Add T 04 Dir and Selection sort file init

---
+++ T 04 Sorting Lvl 1/Selection_Sort.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void selectionSort(int arr[], int n) {
+    for(int i = 0; i < n - 2; i++) {
+        int minimum = i;
+        for(int j = i; j <= n - 1; j++) {
+            if(arr[j] < arr[minimum])
+                minimum = j;
+        }
+        swap(arr[minimum], arr[i]);
+    }
+}
+
+int main(){
+
+    int arr[] = {13, 46, 24, 52, 20, 9};
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    cout << "Before Sorting: ";
+    for(int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+
+    selectionSort(arr, size);
+    cout << endl;
+
+    cout << "After Sorting: ";
+    for(int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
